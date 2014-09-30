9c7cefbb75b916be202561c543560d55e03962c8
Tô Thành Công <tcvn1985@gmail.com>
2014-09-30 21:41:25 +0700

    dat lai bien cho cac card mang

--- devstack-after-install.sh
+++ devstack-after-install.sh
@@ -3,9 +3,14 @@
 ##########################3
 #@ Thuc thi sau khi cai dat openstack xong
 
-eth0_address=`sudo /sbin/ifconfig eth0 | awk '/inet addr/ {print $2}' | cut -f2 -d ":" `
-NETMASK_IP=`sudo /sbin/ifconfig eth0 | awk '/inet addr/ {print $4}' | cut -f2 -d ":"`
-GATEWAY_IP=`sudo ip route list | awk '/^default/ {print $3}'`
+# Dat bien cho eth0
+eth0_ADD=`sudo /sbin/ifconfig eth0 | awk '/inet addr/ {print $2}' | cut -f2 -d ":" `
+eth0_NETMASK_IP=`sudo /sbin/ifconfig eth0 | awk '/inet addr/ {print $4}' | cut -f2 -d ":"`
+eth0_GATEWAY_IP=`sudo ip route list | awk '/^default/ {print $3}'`
+
+# Dat bien cho eth1
+eth1_ADD=`sudo /sbin/ifconfig eth1 | awk '/inet addr/ {print $2}' | cut -f2 -d ":" `
+eth1_NETMASK_IP=`sudo /sbin/ifconfig eth1 | awk '/inet addr/ {print $4}' | cut -f2 -d ":"`
 
 echo "##### Thiet lap IP #####"
 sleep 3
@@ -24,15 +29,15 @@ iface lo inet loopback
 # MGNT NETWORK
 auto eth1
 iface eth1 inet static
-address 10.10.10.160
-netmask 255.255.255.0
+address $eth1_ADD
+netmask $eth1_NETMASK_IP
 
 # EXT NETWORK
 auto br-ex
 iface br-ex inet static
-address $eth0_address
-netmask $NETMASK_IP
-gateway $GATEWAY_IP
+address $eth0_ADD
+netmask $eth0_NETMASK_IP
+gateway $eth0_GATEWAY_IP
 dns-nameservers 8.8.8.8
 
 #
