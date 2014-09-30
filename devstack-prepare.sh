a3296b86eebbdf021f1151b19ad61e992c141f48
Tô Thành Công <tcvn1985@gmail.com>
2014-09-30 16:24:10 +0700

    Cap nhat cau hinh

--- devstack-prepare.sh
+++ devstack-prepare.sh
@@ -2,22 +2,22 @@
 #!/bin/bash -ex
 ####
 
+echo "##### Cai dat devstack #####"
+echo "##### Tao user stack #####"
+sleep 3
 
-# echo "##### Cai dat devstack #####"
-# echo "##### Tao user stack #####"
-# sleep 3
+useradd -m -p Welcome123 stack
+echo "stack ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 
-# useradd -m -p Welcome123 stack
-# echo "stack ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
-
-# echo "##### Phan vung o cung cho SDB #####"
-# sleep 3
-# sudo fdisk -l
-# sudo pvcreate /dev/sdb
-# sudo vgcreate stack-volumes /dev/sdb
+echo "##### Phan vung o cung cho SDB #####"
+sleep 3
+sudo fdisk -l
+sudo pvcreate /dev/sdb
+sudo vgcreate stack-volumes /dev/sdb
 
 echo "##### Tai cac goi can thiet #####"
 sleep 3
+apt-get update -y
 sudo apt-get install git -y || yum install -y git
 
 ##############
@@ -36,20 +36,18 @@ iface lo inet loopback
 # MGNT NETWORK
 auto eth1
 iface eth1 inet static
-address 10.10.10.50
+address 10.10.10.160
 netmask 255.255.255.0
 
 # EXT NETWORK
 auto eth0
 iface eth0 inet static
-address 10.145.47.60
+address 172.16.69.160
 netmask 255.255.255.0
-gateway 10.145.47.1
+gateway 172.16.69.1
 dns-nameservers 8.8.8.8
 
 EOF
 
 init 6
 
-##########
-##########
