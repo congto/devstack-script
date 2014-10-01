f602599a8428f2008ce7a72203bad49218a07e72
Tô Thành Công <tcvn1985@gmail.com>
2014-10-01 15:38:46 +0700

    Update config cho NICs

--- devstack-before.sh
+++ devstack-before.sh
@@ -21,7 +21,17 @@ apt-get update -y
 sudo apt-get install git -y || yum install -y git
 
 ##############
+eth0_ADD=`sudo /sbin/ifconfig eth0 | awk '/inet addr/ {print $2}' | cut -f2 -d ":" `
+eth0_NETMASK_IP=`sudo /sbin/ifconfig eth0 | awk '/inet addr/ {print $4}' | cut -f2 -d ":"`
+eth0_GATEWAY_IP=`sudo ip route list | awk '/^default/ {print $3}'`
+
+# Dat bien cho eth1
+eth1_ADD=`sudo /sbin/ifconfig eth1 | awk '/inet addr/ {print $2}' | cut -f2 -d ":" `
+eth1_NETMASK_IP=`sudo /sbin/ifconfig eth1 | awk '/inet addr/ {print $4}' | cut -f2 -d ":"`
+
 echo "##### Thiet lap IP #####"
+sleep 3
+
 ifaces=/etc/network/interfaces
 test -f $ifaces.orig || cp $ifaces $ifaces.orig
 rm $ifaces
@@ -36,18 +46,28 @@ iface lo inet loopback
 # MGNT NETWORK
 auto eth1
 iface eth1 inet static
-address 10.10.10.160
-netmask 255.255.255.0
+address $eth1_ADD
+netmask $eth1_NETMASK_IP
 
 # EXT NETWORK
 auto eth0
 iface eth0 inet static
-address 172.16.69.160
-netmask 255.255.255.0
-gateway 172.16.69.1
+address $eth0_ADD
+netmask $eth0_NETMASK_IP
+gateway $eth0_GATEWAY_IP
 dns-nameservers 8.8.8.8
 
+#
+# auto eth0
+# iface eth0 inet manual
+# up ifconfig \$IFACE 0.0.0.0 up
+# up ip link set \$IFACE promisc on
+# down ip link set \$IFACE promisc off
+# down ifconfig \$IFACE down
+
 EOF
+
+echo "##### Khoi dong lai may #####"
 
 init 6
 
